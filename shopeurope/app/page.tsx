78e53be76a5e601f2a84540b3a6a35775348d40a
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-03-31 23:32:04 +0100

    first commit

---
+++ shopeurope/app/page.tsx
@@ -0,0 +1,14 @@
+import Container from '@/components/Container';
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const page = () => {
+  return (
+    <Container>
+      <h1>Home</h1>
+      <Button size='lg'>Check Out</Button>
+    </Container>
+  );
+};
+
+export default page;
