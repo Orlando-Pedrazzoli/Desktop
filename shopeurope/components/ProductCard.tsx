bccd25e0f19beadcbe55af8707d2788526218f13
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-04-02 21:57:28 +0100

    Product Card creating

--- shopeurope/components/ProductCard.tsx
+++ shopeurope/components/ProductCard.tsx
@@ -1,12 +1,14 @@
 import { Product } from '@/sanity.types';
 import { urlFor } from '@/sanity/lib/image';
+import { Flame } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <div>
-      <div>
+    <div className='text-sm border-[1px] border-dark_blue/20 rounded-md bg-white group'>
+      <div className='relative group overflow-hidden bg-shop_light_bg'>
         {product?.images && (
           <Image
             src={urlFor(product?.images[0]).url()}
@@ -16,7 +18,30 @@ const ProductCard = ({ product }: { product: Product }) => {
             height={700}
           />
         )}
+        {product?.status === 'sale' && (
+          <p className='absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:text-shop_light_green group-hover:border-shop_light_green hoverEffect'>
+            Sale!
+          </p>
+        )}
+        {product?.status === 'new' && (
+          <p className='absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:text-shop_light_green group-hover:border-shop_light_green hoverEffect'>
+            New!
+          </p>
+        )}
+        {product?.status === 'hot' && (
+          <Link
+            href={'/deal'}
+            className='absolute top-2 left-2 z-10 border border-shop_orange/50 p-1 rounded-full group-hover:border-shop_orange hover:text-shop_dark_green hoverEffect '
+          >
+            <Flame
+              size={18}
+              fill='#fb6c08'
+              className='text-shop_orange/50 group-hover:text-shop_orange hoverEffect'
+            />
+          </Link>
+        )}
       </div>
+      <div className='p-3'>Product Details</div>
     </div>
   );
 };
