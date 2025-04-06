9e61f7ae73c2608c77d4eef49bf6c658a2701886
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-04-06 12:53:35 +0100

    Category page on build

--- shopeurope/app/(client)/page.tsx
+++ shopeurope/app/(client)/page.tsx
@@ -1,17 +1,22 @@
 import Container from '@/components/Container';
 import HomeBanner from '@/components/HomeBanner';
+import HomeCategories from '@/components/HomeCategories';
 import ProductGrid from '@/components/ProductGrid';
+import { getCategories } from '@/sanity/queries';
 import React from 'react';
 
-const page = () => {
+const Home = async () => {
+  const categories = await getCategories(6);
+  console.log(categories);
   return (
     <Container>
       <HomeBanner />
       <div className='py-10'>
         <ProductGrid />
+        <HomeCategories categories={categories} />
       </div>
     </Container>
   );
 };
 
-export default page;
+export default Home;
