9a0454e3f9b8bfb925e40ff864ce42d6c93768d7
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-05-06 10:36:05 +0100

    favourite button setup

--- shopeurope/components/ProductSideMenu.tsx
+++ shopeurope/components/ProductSideMenu.tsx
@@ -1,7 +1,10 @@
 'use client';
 import { cn } from '@/lib/utils';
 import { Product } from '@/sanity.types';
+import useCartStore from '@/store';
 import { Heart } from 'lucide-react';
+import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 
 const ProductSideMenu = ({
   product,
@@ -10,14 +13,33 @@ const ProductSideMenu = ({
   product: Product;
   className?: string;
 }) => {
+  const { favoriteProduct, addToFavorite } = useCartStore();
+  const [existingProduct, setExistingProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    const availableItem = favoriteProduct.find(
+      item => item?._id === product?._id
+    );
+    setExistingProduct(availableItem || null);
+  }, [product, favoriteProduct]);
+
   const handleFavorite = (e: React.MouseEvent<HTMLSpanElement>) => {
-    window.alert('Favorite button clicked');
+    e.preventDefault();
+    if (product?._id) {
+      addToFavorite(product).then(() => {
+        toast.success(
+          existingProduct
+            ? 'Product removed successfully!'
+            : 'Product added successfully!'
+        );
+      });
+    }
   };
   return (
     <div className={cn('absolute top-2 right-2', className)}>
       <div
         onClick={handleFavorite}
-        className={`p-2.5 rounded-full hover:bg-shop_dark_green/80 hover:text-white hoverEffect bg-lightColor}`}
+        className={`p-2.5 rounded-full hover:bg-shop_dark_green/80 hover:text-white hoverEffect ${existingProduct ? 'bg-shop_dark_green/80 text-white' : 'bg-productBg'}`}
       >
         <Heart size={15} />
       </div>
